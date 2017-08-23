8878a0078938aacb175a777b0e56b35636e7e045
Olivia Hugger <olivia@fastmail.com>
2017-08-23 16:07:02 +0200

    cleaneup

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const discord = require('discord.js')
 const search = require('fuzzysearch')
 const speak = require('espeak')
 const path = require('path')
+const readline = require('readline')
 
 const buttons = require('./buttons.json')
 
@@ -15,17 +16,38 @@ const helpMsg = `Commands:
 > __stop__: Stop any music playback.
 `
 
+const responses = [
+  'https://pbs.twimg.com/media/CaEyqFvUkAAdzum.jpg',
+  'https://s-media-cache-ak0.pinimg.com/564x/87/5c/ce/875cce9c0185391b3747ede46b2fb827.jpg',
+  'http://i3.kym-cdn.com/entries/icons/original/000/020/597/CaEyqJJUcAA60xu.jpg',
+  'http://i3.kym-cdn.com/entries/icons/original/000/020/597/CaEyqJJUcAA60xu.jpg',
+  'game grump funny hjjjjjjjjj',
+  'https://vignette4.wikia.nocookie.net/megamitensei/images/3/33/Rise_Kujikawa_render.png/revision/latest?cb=20120401110434',
+  'https://pbs.twimg.com/media/DERI1PZUwAA6_42.jpg'
+]
+
 require('dotenv').config()
 const client = new discord.Client()
 
 client.on('ready', () => {
-  // let channel = client.channels.find(ch => ch.name = 'buzzfeed')
-  // channel.send('aaron carter can suck my dick')
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+  })
+  let channel = client.channels.find(ch => ch.name === 'buzzfeed')
+  rl.on('line', line => {
+    channel.send(line)
+  })
   console.log('arin hanson is ready to die')
 })
 
 client.on('message', message => {
   const regex = new RegExp(`<@!?${process.env.BOT_ID}>`)
+  const hell = /arin(\s+hanson)?/ig
+  if (hell.test(message)) {
+    message.channel.send(responses[Math.floor(Math.random() * responses.length)])
+  }
   if (regex.test(message)) {
     delegate(message)
   }
@@ -53,6 +75,9 @@ function delegate (msg) {
       break
     case 'say':
       say(msg)
+      break
+    case 'die':
+      die(msg)
       break
   }
 }
